57162e14ae47c70400c20dbd8bfb7ae5f604c949
gtiavo <victorg.avila@gmail.com>
2022-04-20 20:08:29 -0300

    db a desarrollo

--- src/db/connections.js
+++ src/db/connections.js
@@ -1,21 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-//Configuracion de Sequelize necesaria para conectar a la db
-// const db = new Sequelize('mundo-disney', 'root', '', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     // logging: false
-// });
-
-const dbName = 'bguegu81ajwbkpppw8dp';
-const user = 'uzrpkf9flvp7rt0d';
-const password = '1WLAfKVZQJRMpGghKRWb';
-const host = 'bguegu81ajwbkpppw8dp-mysql.services.clever-cloud.com'
-
-const db = new Sequelize( dbName, user, password, {
-    host: host,
+// Configuracion de Sequelize necesaria para conectar a la db
+const db = new Sequelize('mundo-disney', 'root', '', {
+    host: 'localhost',
     dialect: 'mysql',
     // logging: false
 });
+
+// const dbName = 'bguegu81ajwbkpppw8dp';
+// const user = 'uzrpkf9flvp7rt0d';
+// const password = '1WLAfKVZQJRMpGghKRWb';
+// const host = 'bguegu81ajwbkpppw8dp-mysql.services.clever-cloud.com'
+
+// const db = new Sequelize( dbName, user, password, {
+//     host: host,
+//     dialect: 'mysql',
+//     // logging: false
+// });
 
 module.exports = db;
